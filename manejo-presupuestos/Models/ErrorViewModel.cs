fed58f156da9d055c83ec7f876c83f8bd05ba83f
Alexis <alexis.duranmachuca@gmail.com>
2023-06-09 09:25:20 -0400

    inicial commit

---
+++ manejo-presupuestos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace manejo_presupuestos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ manejo-presupuestos/Models/TipoCuenta.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace manejo_presupuestos.Models
+{
+    public class TipoCuenta
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(maximumLength:50, MinimumLength = 3, ErrorMessage = "El {0} debe tener entre {2} y {1} caracteres")]
+        public string Nombre { get; set; }
+
+        public int UsuarioId { get; set; }
+        public int Orden { get; set; }
+
+
+    }
+}
